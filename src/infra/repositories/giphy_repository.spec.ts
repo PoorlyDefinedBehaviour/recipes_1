6f246257325a4ac42baae93e398bc6545e325f3f
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-01-04 20:30:46 -0300

    refactor: add more context to giphy repository tests

--- src/infra/repositories/giphy_repository.spec.ts
+++ src/infra/repositories/giphy_repository.spec.ts
@@ -8,37 +8,43 @@ import Gif from "../../domain/entities/gif"
 beforeEach(() => nock.cleanAll())
 
 describe("gif repository unit test suite", () => {
-  test("when http request fails should return Either.left(error)", async () => {
-    nock(`${env.GIF_API_URL}/gifs/search?q=potato`)
-      .get(/.*/)
-      .reply(500, "oops")
-      .persist()
+  describe("findGifsByText", () => {
+    test("when http request fails should return Either.left(error)", async () => {
+      nock(`${env.GIF_API_URL}/gifs/search?q=potato`)
+        .get(/.*/)
+        .reply(500, "oops")
+        .persist()
 
-    const result = await findGifsByText("potato")
+      const result = await findGifsByText("potato")
 
-    expect(isLeft(result)).toBe(true)
-  })
+      expect(isLeft(result)).toBe(true)
+    })
 
-  test("when http request succeeds should return Either.left(error)", async () => {
-    const gifs = [{ url: faker.internet.url() }]
+    test("when http request succeeds should return Either.left(error)", async () => {
+      const gifs = [{ url: faker.internet.url() }]
 
-    nock(`${env.GIF_API_URL}/gifs/search?q=potato`)
-      .get(/.*/)
-      .reply(200, {
-        data: gifs,
-        pagination: { offset: 0, total_count: gifs.length, count: gifs.length },
-        meta: {
-          msg: "",
-          status: 200,
-          response_id: faker.random.alphaNumeric(12),
-        },
-      })
-      .persist()
+      nock(`${env.GIF_API_URL}/gifs/search?q=potato`)
+        .get(/.*/)
+        .reply(200, {
+          data: gifs,
+          pagination: {
+            offset: 0,
+            total_count: gifs.length,
+            count: gifs.length,
+          },
+          meta: {
+            msg: "",
+            status: 200,
+            response_id: faker.random.alphaNumeric(12),
+          },
+        })
+        .persist()
 
-    const result = await findGifsByText("potato")
+      const result = await findGifsByText("potato")
 
-    expect(isRight(result)).toBe(true)
+      expect(isRight(result)).toBe(true)
 
-    expect(getOrElse(() => [] as Gif[])(result)).toEqual(gifs)
+      expect(getOrElse(() => [] as Gif[])(result)).toEqual(gifs)
+    })
   })
 })
