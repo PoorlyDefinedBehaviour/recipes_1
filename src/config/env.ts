751d6d48b1a6d5eb114e2cf8af012bdd2e48db80
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-01-04 20:20:24 -0300

    config(env): add GIF_API_URL to required env keys

--- src/config/env.ts
+++ src/config/env.ts
@@ -40,7 +40,7 @@ const get = (key: string): string => {
   return value
 }
 
-const keys = ["NODE_ENV", "RECIPE_API_URL"] as const
+const keys = ["NODE_ENV", "RECIPE_API_URL", "GIF_API_URL"] as const
 
 export const env = Object.fromEntries(keys.map(key => [key, get(key)])) as {
   [key in typeof keys[number]]: string
