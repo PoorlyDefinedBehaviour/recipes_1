d4f26dab7a12858fb1801282c46f659ba531d88a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-01-05 22:06:59 -0300

    refactor(domain/repositories): RecipeRepository.findRecipesByKeywords returns Promise<Either<Error, Recipe[]>> instead of Promise<Either<Failure, Recipe[]>>

--- src/domain/repositories/recipe_repository.ts
+++ src/domain/repositories/recipe_repository.ts
@@ -1,9 +1,8 @@
 import { Either } from "fp-ts/lib/Either"
-import { Failure } from "../contracts/failure"
 import Recipe from "../entities/recipe"
 
 export interface RecipeRepository {
   findRecipesByKeywords: (
     keywords: string[]
-  ) => Promise<Either<Failure, Recipe[]>>
+  ) => Promise<Either<Error, Recipe[]>>
 }
